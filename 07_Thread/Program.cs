a418f4cefe85e7102e6ec98761a3d7d326eba464
João Denilson S. dos Santos <denilson_santos@virgo.atlantico.net.br>
2023-01-24 08:40:51 -0300

    AutoManual

---
+++ 07_Thread/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+namespace _07_Thread
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("StarDate: " + DateTime.Now);
+
+            for (int i = 0; i < 5; i++)
+            {
+                Thread t1 = new Thread(ThreadLoap);
+                t1.Start();
+
+            }
+
+            Console.ReadKey();
+        }
+        static void ThreadLoap()
+        {
+            for (int i=0; i<100; i++)
+            {
+                Console.WriteLine("Num: "+i);
+            }
+        }
+    }
+}
---
+++ 08_AutoManual/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _08_AutoManual
+{
+    internal class Program
+    {
+        static ManualResetEvent manua01;
+        static AutoResetEvent auto01;
+
+        static void Main(string[] args)
+        {
+            manua01= new ManualResetEvent(false);
+            auto01 = new AutoResetEvent(false);
+
+            //Thread t1 = new Thread(Execution01);
+            //t1.Start();
+            Thread t2 = new Thread(Execution02);
+            t2.Start();
+
+
+            Thread.Sleep(5000);
+            manua01.Set();
+            manua01.Reset();
+
+            auto01.Set();
+
+            Thread.Sleep(5000);
+            manua01.Set();
+            auto01.Set();
+
+            Console.ReadKey();
+        }
+        static void Execution01()
+        {
+            Console.WriteLine("01 - Iniciando executa 01");
+            manua01.WaitOne();
+            Console.WriteLine("02 - Em execução 01 Executa 01");
+            Console.WriteLine("03 - Em execução 02 Executa 01");
+            manua01.WaitOne();
+            Console.WriteLine("04 - Finalizando 01");
+        }
+
+        static void Execution02()
+        {
+            Console.WriteLine("01 - Iniciando executa 02");
+            auto01.WaitOne();
+            Console.WriteLine("02 - Em execução 01 Executa 02");
+            Console.WriteLine("03 - Em execução 02 Executa 02");
+            auto01.WaitOne();
+            Console.WriteLine("04 - Finalizando 02");
+        }
+    }
+}
